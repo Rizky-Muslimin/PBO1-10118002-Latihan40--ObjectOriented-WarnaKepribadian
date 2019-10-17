e89311df143d4ef6477649faa6afa6ce84f1efc8
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-17 20:39:17 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan40-(ObjectOriented)WarnaKepribadian/src/pbo1/pkg10118002/latihan40/objectoriented/warnakepribadian/PBO110118002Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan40.objectoriented.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rizky Muslimin
+ * KELAS    : IF-1
+ * NIM      : 10118002
+ * Deskripsi Program : Program ini untuk menampilkan tampilan dari warna dan 
+ *                     kepribadian.
+ */
+public class PBO110118002Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
